7c55eea56ba766ed93d22c2efda1c69eb1521eef
divyam0106 <divyam.mathur@neuriot.com>
2021-12-16 13:05:54 +0530

    test

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const PORT = 3000
 
 require("dotenv")
 
+app.get("/getAccess",(req,res)=>{
+    console.log(process.env.ACCESSKEY,process.env.ACCESSID);
+})
+
 app.listen(PORT,  () =>{
     console.log("server running");
 });
