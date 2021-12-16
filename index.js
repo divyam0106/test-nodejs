9ac84edea5836c8a43e90e3195f5dea83586426e
divyam0106 <divyam.mathur@neuriot.com>
2021-12-16 13:09:29 +0530

    test

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const PORT = 3000
 require("dotenv")
 
 app.get("/getAccess",(req,res)=>{
-    console.log(process.env.ACCESSKEY,process.env.ACCESSID);
+    console.log("inside get req",process.env.ACCESSKEY,process.env.ACCESSID);
+    res.send("hello")
 })
 
 app.listen(PORT,  () =>{
