38aad04e097dd67be44f99888730a7f686ba3f10
divyam0106 <divyam.mathur@neuriot.com>
2021-12-16 14:26:43 +0530

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 app.get("/getAccess",(req,res)=>{
-    console.log("inside get req",process.env.ACCESSKEY,process.env.ACCESSID);
+    console.log("inside get req",process.env, process.env.NODE_VERSION,process.env.YARN_VERSION);
     res.send("hello")
 })
 
